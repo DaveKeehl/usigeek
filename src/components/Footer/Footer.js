496bfa61f0dabe821ff1a8f9939f56730859da47
Davide <davide.ciulla@ticino.com>
2019-12-31 02:06:37 +0100

    some more general style update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,16 @@ class Footer extends Component {
 	render() {
 		return (
 			<footer className={this.props.className}>
-				<div className="socials"></div>
+				<div className="socials">
+					<a href="https://www.facebook.com/UsiGeek/"></a>
+					<a href="https://www.facebook.com/UsiGeek/"></a>
+					<a href="https://www.facebook.com/UsiGeek/"></a>
+					<a href="https://www.facebook.com/UsiGeek/"></a>
+					<a href="https://www.facebook.com/UsiGeek/"></a>
+					<a href="https://www.facebook.com/UsiGeek/"></a>
+				</div>
 				<p>© 2020 USI Geek. Tutti i diritti riservati.</p>
-				<p>Sviluppato da Davide Ciulla</p>
+				<p>Sviluppato da <a href="https://www.google.com/">Davide Ciulla</a></p>
 			</footer>
 		)
 	}
