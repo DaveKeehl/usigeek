b0402fc6c7b64657a6daf404cb5f3f28b25accb1
Davide <davide.ciulla@ticino.com>
2019-12-31 13:48:10 +0100

    Added mouse in hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Logo from '../../images/logo.svg';
+import Arrows from '../../images/arrows.svg';
 
 class Hero extends Component {
 	render() {
 		return (
 			<div className={this.props.className}>
-				<img src={Logo} alt="Logo"></img>
+				<img className="logo" src={Logo} alt="Logo"></img>
+				<div className="scroll">
+					<div className="mouse"></div>
+					<img src={Arrows} alt="Arrows"></img>
+				</div>
 			</div>
 		)
 	}
