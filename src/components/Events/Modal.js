a4f60a13d8e4b54496eaeb3728e86f35642082bb
Loris Bettazza <loris.bettazza@gmail.com>
2020-08-18 21:23:16 +0200

    Allow modal navigation using keyboad
    ESC - close modal
    LEFT - previous image
    RIGHT - next image

--- src/components/Events/Modal.js
+++ src/components/Events/Modal.js
@@ -1,17 +1,33 @@
-import React, {useState} from "react"
+import React, {useState, useEffect, useCallback} from "react"
 import close from "../../images/close.svg"
 import arrow from "../../images/arrow.svg"
 
+const ESC_KEY = 27
+const LEFT_ARROW_KEY = 37
+const RIGHT_ARROW_KEY = 39
+
 function Modal(props) {
 	const [counter, setCounter] = useState(0)
+	const { closeModal } = props;
 
-	const back = () => {
+	const back = useCallback(() => {
 		setCounter(prevState => (prevState === 0 ? props.photos.length-1 : prevState - 1))
-	}
+	}, [props.photos])
 
-	const next = () => {
+	const next = useCallback(() => {
 		setCounter(prevState => (prevState === props.photos.length-1 ? 0 : prevState + 1))
-	}
+	}, [props.photos])
+
+	useEffect(() => {
+		const keyDownHandler = e => {
+			if (e.keyCode === ESC_KEY) closeModal()
+			else if (e.keyCode === LEFT_ARROW_KEY) back()
+			else if (e.keyCode === RIGHT_ARROW_KEY) next()
+		}
+
+		document.addEventListener('keydown', keyDownHandler)
+		return () => document.removeEventListener('keydown', keyDownHandler)
+	}, [closeModal, back, next])
 
 	return (
 		<div className="modal" style={props.isOpen ? {display: "grid"} : {display: "none"}}>
@@ -25,7 +41,7 @@ function Modal(props) {
 				src={close}
 				className="modal-close" 
 				alt="Close modal window"
-				onClick={() => props.closeModal()}
+				onClick={closeModal}
 			/>
 			<div style={props.photos.length <= 1 ? {display: "none"} : null}>
 				<img 
